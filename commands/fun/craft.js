edb971ec48acf0519c5f861dabf6a42568a6fd79
scar17off <scar17off@gmail.com>
2024-05-05 23:23:50 +0300

    Several enhancements have been made to the crafting code

--- commands/fun/craft.js
+++ commands/fun/craft.js
@@ -1,51 +1,56 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { EmbedBuilder } = require('discord.js');
 const axios = require('axios');
+
+const maximumIngredients = 5;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('craft')
-        .setDescription('Crafts a new item using two ingredients')
+        .setDescription('Crafts a new item using ingredients separated by " + ". You can combine up to 5 ingredients.')
         .addStringOption(option =>
-            option.setName('first')
-                .setDescription('The first ingredient')
-                .setRequired(true))
-        .addStringOption(option =>
-            option.setName('second')
-                .setDescription('The second ingredient')
+            option.setName('ingredients')
+                .setDescription('List of ingredients separated by " + "')
                 .setRequired(true)),
     async execute(interaction) {
-        const firstIngredient = interaction.options.getString('first');
-        const secondIngredient = interaction.options.getString('second');
+        const ingredientsInput = interaction.options.getString('ingredients');
+        const ingredients = ingredientsInput.split(' + ').slice(0, maximumIngredients);
+
+        if (ingredients.length < 2) {
+            return interaction.reply({ content: 'Please provide at least two ingredients separated by " + ".', ephemeral: true });
+        }
 
         try {
-            const response = await axios.get(`https://neal.fun/api/infinite-craft/pair`, {
-                params: { first: firstIngredient, second: secondIngredient },
-                headers: {
-                    'sec-ch-ua': `"Chromium";v="124", "Microsoft Edge";v="124", "Not-A.Brand";v="99"`,
-                    'accept-language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,be;q=0.6',
-                    'sec-ch-ua-mobile': '?0',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
-                    'accept': '*/*',
-                    'Referer': 'https://neal.fun/infinite-craft/',
-                    'priority': 'u=1, i',
-                    'sec-ch-ua-platform': '"Windows"'
-                }
-            });
+            let result = ingredients[0];
+            let resultMessage = '';
+            let emoji = '';
 
-            const result = response.data.result;
-            const isNewDiscovery = response.data.isNew;
-            const emoji = response.data.emoji;
+            for (let i = 0; i < ingredients.length - 1; i++) {
+                const firstIngredient = result;
+                const secondIngredient = ingredients[i + 1];
 
-            const embed = new EmbedBuilder()
-                .setColor(0xA312ED)
-                .setTitle('Crafting Result')
-                .addFields(
-                    { name: 'Result', value: `${firstIngredient} + ${secondIngredient} = ${emoji} ${result}`, inline: false },
-                    { name: 'New discovery?', value: isNewDiscovery ? ':white_check_mark:' : ':negative_squared_cross_mark:', inline: false }
-                );
+                const response = await axios.get(`https://neal.fun/api/infinite-craft/pair`, {
+                    params: { first: firstIngredient, second: secondIngredient },
+                    headers: {
+                        'sec-ch-ua': `"Chromium";v="124", "Microsoft Edge";v="124", "Not-A.Brand";v="99"`,
+                        'accept-language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,be;q=0.6',
+                        'sec-ch-ua-mobile': '?0',
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
+                        'accept': '*/*',
+                        'Referer': 'https://neal.fun/infinite-craft/',
+                        'priority': 'u=1, i',
+                        'sec-ch-ua-platform': '"Windows"'
+                    },
+                    timeout: 15000
+                });
 
-            await interaction.reply({ embeds: [embed] });
+                result = response.data.result;
+                emoji = response.data.emoji;
+                const isNewDiscovery = response.data.isNew;
+
+                resultMessage += `${firstIngredient} + ${secondIngredient} = ${emoji} ${result} ${isNewDiscovery ? 'NEW!' : ''}\n`;
+            }
+
+            await interaction.reply({ content: resultMessage.trim() });
         } catch (error) {
             console.error('Error with crafting API:', error);
             await interaction.reply({ content: 'Failed to craft the items. Please try again later.', ephemeral: true });
